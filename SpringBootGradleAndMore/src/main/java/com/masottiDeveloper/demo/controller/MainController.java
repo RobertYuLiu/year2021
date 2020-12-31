cd2d7035c1f03b3dcc0f4257b637e941255e38c9
RobertYuLiu <liuyu_canada@hotmail.com>
2020-12-30 19:14:40 -0500

    1st commit

---
+++ SpringBootGradleAndMore/src/main/java/com/masottiDeveloper/demo/controller/MainController.java
@@ -0,0 +1,32 @@
+package com.masottiDeveloper.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MainController
+{
+
+    @GetMapping(path = "/hello", produces=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> sayHello()
+    {
+         //Get data from service layer into entityList.
+
+        List<JSONObject> entities = new ArrayList<JSONObject>();
+//        for (Entity n : entityList) {
+            JSONObject entity = new JSONObject();
+            entity.put("aa", "bb");
+            entities.add(entity);
+//        }
+        return new ResponseEntity<Object>(entities, HttpStatus.OK);
+    }
+}
